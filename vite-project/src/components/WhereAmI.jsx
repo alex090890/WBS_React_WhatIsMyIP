0f2ac7b614138a531b775f72ed19f761de250e56
Oleksandr Lazurenko <alexprofteach@gmail.com>
2024-02-24 15:49:54 +0100

    Worked on the components

---
+++ vite-project/src/components/WhereAmI.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+export default function WhereAmI() {
+  const [location, setLocation] = useState('');
+  const [country, setCountry] = useState('');
+  const [flagUrl, setFlagUrl] = useState(''); // Add a new state for the flag URL
+
+  const getLocation = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setLocation("Your latitude: " + position.coords.latitude + ". Your longitude: " +  position.coords.longitude + ".");
+      fetchCountry(position.coords.latitude, position.coords.longitude);
+    });
+  };
+
+  const fetchCountry = (latitude, longitude) => {
+    fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`)
+      .then(response => response.json())
+      .then(data => {
+        const countryCode = data.countryCode;
+        fetch(`https://restcountries.com/v3.1/alpha/${countryCode}`)
+          .then(response => response.json())
+          .then(countryData => {
+            const countryName = countryData[0].name.common;
+            setCountry(countryName);
+            setFlagUrl(countryData[0].flags.svg); // Set the flag URL
+          })
+          .catch(error => console.error("Error fetching country:", error));
+      })
+      .catch(error => console.error("Error fetching location:", error));
+  };
+
+  return (
+    <div>
+      <h2>Your location: </h2>
+      <button onClick={getLocation}>Get location</button>
+      <div>{location}</div>
+      {country && (
+        <div>
+          <div>Your country: {country}</div>
+        <div>{flagUrl && (<img className="locationimg" src={flagUrl} alt={`${country} flag`} width="50" />
+          )}</div>
+        </div>
+      )}
+    </div>
+  );
+}
